16a052a11956b1527f7a2fb738cbe61d0912c31f
Artyom K <hotkosc@gmail.com>
2017-11-09 21:56:24 +0300

    Add emacs config

---
+++ init.el
@@ -0,0 +1,19 @@
+(package-initialize)
+
+(require 'package)
+
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+
+(show-paren-mode 2)
+(custom-set-variables
+ '(custom-enabled-themes (quote (wombat)))
+ '(package-selected-packages
+   (quote
+    (github-issues company-anaconda jabber))))
+
+; evil mode
+(require 'evil)
+(evil-mode 1)
+
+; Python settings
+(add-hook 'python-mode-hook 'anaconda-mode)
