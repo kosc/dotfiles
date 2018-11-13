78ed826eddfbfb0bfd48889a659c3c94e3573394
Kosenko Artem <kosenko.artem@220-volt.ru>
2018-11-13 17:43:04 +0300

    Add GPG key and IN_PROGRESS status
    * Add default GPG key for encrypting my org-files
    * Add IN_PROGRESS status for org-mode TODO-lists

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'package)
 
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
@@ -15,3 +22,9 @@
 
 ; Python settings
 (add-hook 'python-mode-hook 'anaconda-mode)
+
+; Use main GPG key by default
+(setq epa-file-select-keys "6163B960A5CC15D0")
+
+(setq org-todo-keywords
+  '((sequence "TODO" "IN_PROGRESS" "|" "DONE")))
